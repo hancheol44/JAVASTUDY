ac40cfed81161d2306f9b189d460e4586a2e826f
lhc <lhc@DESKTOP-NDIGIRG>
2020-04-09 01:09:15 +0900

    commit  test2

---
+++ Study/src/test2/Ex01.java
@@ -0,0 +1,19 @@
+package test2;
+
+import java.util.*;
+public class Ex01 {
+
+	public static void main(String[] args) {
+//		Scanner scanner = new Scanner(System.in);
+//		int minute = scanner.nextInt();
+//		System.out.println("입력한"+minute+"분을 초단위로 출력하면 "+minute*60+"초 입니다");
+		
+		
+		int a = 10;
+		int b = a++ + a++;
+		System.out.println(b);
+		
+		
+	}
+
+}
---
+++ Study/src/test2/Ex10.java
@@ -0,0 +1,17 @@
+package test2;
+
+import java.util.*;
+public class Ex10 {
+
+	public static void main(String[] args) {
+		ArrayList<String> hero = new ArrayList<String>();
+		hero.add("박태환");
+		hero.add("김연아");
+		hero.add("손연재");
+		
+		for(String k : hero) {
+			System.out.println(k);
+		}
+	}
+
+}
